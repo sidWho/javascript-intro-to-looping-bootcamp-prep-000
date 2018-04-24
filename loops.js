1dc7a2b5a817daf8f32789c4b06010783af8f69d
NAVDEEP SIDHU <sidhu44@gmail.com>
2018-04-24 05:53:44 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -20,12 +20,13 @@ function whileLoop(number){
 
 
 function doWhileLoop(array){
+  function maybeTrue() {
+  return Math.random() >= 0.5 // Returns a random number between 0 (inclusive) and 1 (exclusive)
+}
   do{
     array.delete
   } while (array.length > 0 && maybeTrue())
   return array
 }
 
-function maybeTrue() {
-  return Math.random() >= 0.5 // Returns a random number between 0 (inclusive) and 1 (exclusive)
-}
+
